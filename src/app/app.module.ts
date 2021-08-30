506368d1f8da098f3614552f5df478dec7f6d0cf
Michael Coons <mcoons67@gmail.com>
2021-08-30 07:35:40 -0600

    CSS changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MainnavComponent } from './mainnav/mainnav.component';
 import { HomeComponent } from './home/home.component';
-import { ArticlesComponent } from './space/articles/articles.component';
-import { PicturesComponent } from './space/pictures/pictures.component';
-import { VideosComponent } from './space/videos/videos.component';
+// import { ArticlesComponent } from './space/articles/articles.component';
+// import { PicturesComponent } from './space/pictures/pictures.component';
+// import { VideosComponent } from './space/videos/videos.component';
 import { FrameComponent } from './frame/frame.component';
 import { ApplicationPipesModule } from './ApplicationPipes/ApplicationPipes.module';
 import { AboutComponent } from './about/about.component';
@@ -27,9 +27,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent,
     MainnavComponent,
     HomeComponent,
-    ArticlesComponent,
-    PicturesComponent,
-    VideosComponent,
+    // ArticlesComponent,
+    // PicturesComponent,
+    // VideosComponent,
     FrameComponent,
     AboutComponent,
     AppcardComponent,
